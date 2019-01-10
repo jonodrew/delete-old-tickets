fa477c0c434911f3a31ae45e156718d78d0fee59
Jonathan Kerr <jonathan.kerr@digital.cabinet-office.gov.uk>
2019-01-10 10:43:23 +0000

    This commit wraps everything up in functions
    I've further tidied up some code, putting it into functions. There are two big functions that look really similar though, so I'm going to see if I can turn them into one function that's more generic...

--- gdpr_zendesk.py
+++ gdpr_zendesk.py
@@ -15,9 +15,6 @@ date=(date_ago.strftime("%Y-%m-%d"))
 print("Gathering records from before: {}".format(date))
 
 # Access API with search criteria
-params = {'query': 'type:ticket solved<{}'.format(date)}
-
-url = "https://cabinetoffice.zendesk.com/api/v2/search.json?query={}".format(urlencode(params))
 
 def get_next_page(url, user, pwd):
     response = requests.get(url, auth=(user, pwd))
@@ -40,79 +37,50 @@ def delete_ticket(ticket, user, pwd):
 ### this works below!
 # The below code pulls ticket ids and the last updated dates of all tickets older than the date specified in the API search above
 # and places in a csv file.
+def soft_delete(user, pwd, date):
+    """
+    This function does a "soft delete". The ticket is marked as deleted, but
+    still exists in Zendesk.
+    """
+    params = {'query': 'type:ticket solved<{}'.format(date)}
+    url = "https://cabinetoffice.zendesk.com/api/v2/search.json?query={}".format(urlencode(params))
+    data = get_next_page(url, user, pwd)
+    #while loop to proceed through pages of tickets pulled from API
+    while data['next_page'] != "null":
+        for ticket in data['results']:
+            delete_ticket(ticket, user, pwd)
+            #write ticket ids and dates to csv for records
+            row = [id, ticketurl, updated, "deleted"]
+            with open('oldtickets {}.csv'.format(datetime.now()), 'a') as oldtickets:
+                writer = csv.writer(oldtickets)
+                writer.writerow(row)
 
-#while loop to proceed through pages of tickets pulled from API
-while data['next_page'] != "null":
-    # Do the HTTP get request
-    response = requests.get(url, auth=(user, pwd))
-
-    # Check for HTTP codes other than 200
-    if response.status_code != 200:
-        print('Status:', response.status_code, 'Problem with the request. Exiting.')
-        exit()
-    # Decode the JSON response into a dictionary and use the data
-    data = response.json()
-
-    for ticket in data['results']:
-        id = ticket['id']
-        ticketurl = ticket['url']
-        updated = ticket['updated_at']
-        print("ticket id: {}".format(id))
-        print("Ticket last updated: {}".format(updated))
-
-        #delete the ticket from Zendesk
-        requests.delete(ticketurl, auth=(user, pwd))
-
-        #write ticket ids and dates to csv for records
-        row = [id, ticketurl, updated, "deleted"]
-        with open('oldtickets {}.csv'.format(datetime.now()), 'a') as oldtickets:
-            writer = csv.writer(oldtickets)
-            writer.writerow(row)
-
-    #find the next url to pull tickets from
-    url=data['next_page']
-
+        #find the next url to pull tickets from
+        url=data['next_page']
+        data = get_next_page(url, user, pwd)
+    return True
 
 #Permanently/hard delete tickets from the API
 
+def hard_delete(user, pwd):
+    durl = "https://cabinetoffice.zendesk.com/api/v2/deleted_tickets.json"
 
-durl = "https://cabinetoffice.zendesk.com/api/v2/deleted_tickets.json"
+    ddata = get_next_page(durl, user, pwd)
 
-dresponse = requests.get(durl, auth=(user, pwd))
+    #while loop to proceed through pages of deleted tickets pulled from API
+    while ddata['next_page'] != "null":
+        for ticket in ddata['deleted_tickets']:
+            delete_ticket(ticket, user, pwd)
 
-if dresponse.status_code != 200:
-    print('Status:', dresponse.status_code, 'Problem with the request. Exiting.')
-    exit()
+             #write ticket ids and dates to csv for records
+            row = [did, dticketurl, deleted, "hard-deleted"]
+            with open('harddelete {}.csv'.format(datetime.now()), 'a') as harddelete:
+                writer = csv.writer(harddelete)
+                writer.writerow(row)
 
-# Decode the JSON response into a dictionary and use the data
-ddata = dresponse.json()
+        #find the next url to pull tickets from
+        durl=ddata['next_page']
 
-
-#while loop to proceed through pages of deleted tickets pulled from API
-while ddata['next_page'] != "null":
-    for ticket in ddata['deleted_tickets']:
-        did = ticket['id']
-        dticketurl = ('https://cabinetoffice.zendesk.com/api/v2/deleted_tickets/{}.json'.format(did))
-        deleted = ticket['deleted_at']
-        print("ticket id: {}".format(did))
-        print("Ticket last deleted: {}".format(deleted))
-
-        #hard delete the ticket from Zendesk
-        requests.delete(dticketurl, auth=(user, pwd))
-
-         #write ticket ids and dates to csv for records
-        row = [did, dticketurl, deleted, "hard-deleted"]
-        with open('harddelete {}.csv'.format(datetime.now()), 'a') as harddelete:
-            writer = csv.writer(harddelete)
-            writer.writerow(row)
-
-    #find the next url to pull tickets from
-    durl=ddata['next_page']
-
-    #Go to next page of tickets
-    dresponse = requests.get(durl, auth=(user, pwd))
-    if dresponse.status_code != 200:
-        print('Status:', dresponse.status_code, 'Problem with the request. Exiting.')
-        exit()
-    data = dresponse.json()
-    #repeat - go back to begnning of loop for next page of tickets
+        #Go to next page of tickets
+        get_next_page(durl, user, pwd)
+        #repeat - go back to begnning of loop for next page of tickets
